5d5da01e5d7ed63180abf4f416c726530c0b31bf
Izukripto <kjrwesley21@gmail.com>
2023-08-17 09:45:13 +0100

    Revert "err connecting to mongodb"
    This reverts commit 1dca69eafc40e259d9a62abaf5847e0c6a42a45f.
    exposed mongo URI

--- server/server.js
+++ server/server.js
@@ -10,11 +10,6 @@ app.use(express.json());
 //connect to server
 const PORT = process.env.PORT || 5500;
 
-//connect to mongodb
-mongoose.connect(process.env.DB_CONNECT)
-.then(()=> console.log('Database Connected'))
-.catch(err => console.log(err))
-
 
 
 
